bb2ef2da3dc75e7862527ea6c63239a6d139f1b5
Gregory Wickstrom <gwick001@ucr.edu>
2017-05-08 17:24:43 -0700

    postgresql added

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
-
+//new stuff below
+var pg = require('pg');
+//new stuff above
 var server = app.listen(8000, function() {
 	console.log("listening on port 8000");
 });
@@ -10,6 +12,20 @@ app.use(express.static(path.join(__dirname, './client')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+
+//new stuff below
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+//new stuff above
 app.set('views', path.join(__dirname, './client'));
 app.set('view engine', 'ejs');
 
