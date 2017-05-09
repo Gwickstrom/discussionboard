5995ebb743f52be573590f46698850519873afa3
Gregory Wickstrom <gwick001@ucr.edu>
2017-05-08 18:10:43 -0700

    new mongodb URI

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 var fs = require("fs");
 
-mongoose.connect(process.env.mongodb:heroku_s34n8lc3:h92utn9uf7shm2c1riuei4026f@ds133221.mlab.com:33221/heroku_s34n8lc3);
+mongoose.connect(process.env.heroku_s34n8lc3:h92utn9uf7shm2c1riuei4026f@ds133221.mlab.com:33221/heroku_s34n8lc3);
 // mongoose.connect("mongodb://localhost/discussion_board3");
 
 var models_path = __dirname + "/../server/models";
